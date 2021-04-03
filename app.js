e74a81ce10e36708726a2be4537de6560c507ca2
Sidney Robhez <sidney.robhez@gmail.com>
2021-04-02 22:57:43 -0600

    Initial Files

---
+++ app.js
@@ -0,0 +1,25 @@
+//traversing the DOM
+	// const btns = document.querySelectorAll(".question-btn");
+
+	// btns.forEach(function(btn) {
+	// 	btn.addEventListener("click", function(event) {
+	// 		const question = event.currentTarget.parentElement.parentElement;
+	// 		question.classList.toggle("show-text");
+	// 	});
+	// });
+//selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+	//console.log(question);
+	const btn = question.querySelector('.question-btn');
+	//console.log(btn);
+	btn.addEventListener("click", function() {
+		questions.forEach(function(item) {
+			if(item !== question) {
+				item.classList.remove("show-text");
+			}
+		})
+		question.classList.toggle("show-text");
+	})
+});
